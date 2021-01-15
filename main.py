17e3605f2cd0b93861731e757234106497e2e779
aroquev00 <43384970+aroquev00@users.noreply.github.com>
2021-01-15 03:56:22 +0000

    Add source code

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import requests
+import os
+from datetime import date
+import time
 
-print("Hi")
+# Get the day's full date
+today = date.today()
 
+# Format the date to be used in the pdf file name
+dateFormatted = today.strftime("%d-%m-%Y")
+
+# Get the full path of the script
+path = os.path.dirname(os.path.realpath(__file__))
+
+# Create the pdf name
+filename = path + "/docs/pemex-" + dateFormatted + ".pdf"
+
+# Get the urls
 mainUrl = 'https://www.comercialrefinacion.pemex.com/portal/menu/controlador?Destino=menu_gral.jsp'
 pdfUrl = 'https://www.comercialrefinacion.pemex.com/portal/scgli004/controlador?Destino=MuestraPDF&doctoID=1'
 
+# Start a session
 s = requests.Session()
+
+# Request main page to get cookies and headers
 mainReq = s.get(mainUrl)
+
+# Maybe unnecessary, but may allow the server to catch up before next request?
+time.sleep(1)
+
+# Make request for PDF file
 pdfReq = s.get(pdfUrl)
 
-print(mainReq.text)
-print(s.cookies)
-print(pdfReq.text)
+# Write PDF file to filesystem
+with open(filename, 'wb') as f:
+  f.write(pdfReq.content)
