1d02809f6ab3699c4668b5baba15f58ebc2fc20d
aroquev00 <43384970+aroquev00@users.noreply.github.com>
2021-01-15 00:01:31 +0000

    Get pdf text

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import requests
 
 print("Hi")
+
+mainUrl = 'https://www.comercialrefinacion.pemex.com/portal/menu/controlador?Destino=menu_gral.jsp'
+pdfUrl = 'https://www.comercialrefinacion.pemex.com/portal/scgli004/controlador?Destino=MuestraPDF&doctoID=1'
+
+s = requests.Session()
+mainReq = s.get(mainUrl)
+pdfReq = s.get(pdfUrl)
+
+print(mainReq.text)
+print(s.cookies)
+print(pdfReq.text)
